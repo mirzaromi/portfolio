a1129b6cb297fa6198ca608f60c974ca0d44d5cf
mirzaromi <mirzaromi12@gmail.com>
2022-07-05 17:40:32 +0700

    sudah ditambahkan javascript

---
+++ script.js
@@ -0,0 +1,19 @@
+const hamburger = document.querySelector('#hamburger');
+const close = document.querySelector('#close');
+const mobileMenu = document.querySelector('.mobile-menu');
+console.log(mobileMenu);
+
+const hamburgerClick = () => {
+        mobileMenu.style.left = '0px';
+        hamburger.style.display = 'none';
+        close.style.display = 'block';
+}
+
+const closeClick = () => {
+        mobileMenu.style.left = '-255px';
+        hamburger.style.display = 'block';
+        close.style.display = 'none';
+}
+
+hamburger.addEventListener("click", hamburgerClick);
+close.addEventListener("click", closeClick);
